189a07a60cb177b4510518f10ea974115f475598
jwandekoken <jwandekoken@gmail.com>
2025-11-04 06:37:10 -0300

    refactor: remove unused import

--- apps/web/src/stores/auth.ts
+++ apps/web/src/stores/auth.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { ref, computed, watch } from "vue";
+import { ref, computed } from "vue";
 import { useSessionStorage } from "@vueuse/core";
 import { useRouter } from "vue-router";
 import {
